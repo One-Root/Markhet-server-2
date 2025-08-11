331ef196cea044a293074ee19eea32ade9761203
shahnoor-oneRoot <shahnoor@oneroot.farm>
2025-08-11 12:36:37 +0530

    fix: uncomment Redis username and password configuration in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -51,8 +51,8 @@ import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
       connection: {
         host: process.env.REDIS_HOST || 'localhost',
         port: parseInt(process.env.REDIS_PORT) || 6379,
-        // username: process.env.REDIS_USERNAME || 'default',
-        // password: process.env.REDIS_PASSWORD || 'password',
+        username: process.env.REDIS_USERNAME || 'default',
+        password: process.env.REDIS_PASSWORD || 'password',
       },
     }),
 
